c95e0b126f0ec38c142525e9c497eaac5800cf70
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-24 23:38:08 +0300

    added User profile, rating and project to the admin

--- z_awwards/admin.py
+++ z_awwards/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile, UserProject, ProjectRating
+
+admin.site.register(UserProfile)
+admin.site.register(UserProject)
+admin.site.register(ProjectRating)
