c91acc911ca199211e6f274fb4f2827a48e6c806
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-25 00:13:09 +0300

    created forms for updating users, projects and ratings

--- z_awwards/forms.py
+++ z_awwards/forms.py
@@ -41,3 +41,49 @@ class ProjectRatingForm(ModelForm):
             'usability': 'Usability',
             'content': 'Content'
         }
+
+
+class UpdateUserForm(ModelForm):
+    """
+    Form for updating user information.
+    """
+    email = forms.EmailField(required=True, label='Email', max_length=150, help_text='Enter valid email address')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateUserProfileForm(ModelForm):
+    """
+    Form for updating user profile.
+    """
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+
+class UpdateProjectForm(ModelForm):
+    """
+    Form for updating project information.
+    """
+
+    class Meta:
+        model = UserProject
+        fields = ('title', 'description', 'url', 'photo', 'technologies')
+
+
+class UpdateRatingForm(ModelForm):
+    """
+    Form for updating project rating.
+    """
+
+    class Meta:
+        model = ProjectRating
+        fields = ['design', 'usability', 'content']
+        labels = {
+            'design': 'Design',
+            'usability': 'Usability',
+            'content': 'Content'
+        }
