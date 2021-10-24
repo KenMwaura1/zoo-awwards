56009a18bdce9093c74cd24376a7c089c94ddc06
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-25 00:55:22 +0300

    updated main home route with form and projects new methods for project model

--- z_awwards/forms.py
+++ z_awwards/forms.py
@@ -6,7 +6,7 @@ from django import forms
 from z_awwards.models import UserProject, ProjectRating
 
 
-class SignupForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
