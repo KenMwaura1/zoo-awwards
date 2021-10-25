bbf8897d7f7780216a98798c996cb29c831b5b7e
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-25 03:53:18 +0300

    Added urls for the app

--- z_awwards/urls.py
+++ z_awwards/urls.py
@@ -1,7 +1,25 @@
 from django.urls import include, path
+from rest_framework import routers
 
 from z_awwards import views
 
+
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('projects', views.ProjectViewSet)
+router.register('profiles', views.ProfileViewSet)
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('user_profile/<username>/settings', views.edit_profile, name='edit_profile'),
+    path('project/<project>', views.single_project, name='single_project'),
+    path('user_profile/<username>/projects', views.user_projects, name='user_projects'),
+    path('search/', views.search_projects, name='search'),
+
+
     ]
