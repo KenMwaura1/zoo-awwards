8e442f9eec336333473a99b8d481bb158291323a
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-25 03:14:34 +0300

    added route to get projects for user and edit profile

--- z_awwards/views.py
+++ z_awwards/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
-from z_awwards.forms import ProjectForm, RegisterForm
-from z_awwards.models import UserProject
+from z_awwards.forms import ProjectForm, RegisterForm, UpdateUserForm, UpdateUserProfileForm, ProjectRatingForm
+from z_awwards.models import UserProject, ProjectRating
 
 
 def home(request):
@@ -56,9 +58,45 @@ def profile(request, username):
     return render(request, 'z_awwards/profile.html')
 
 
-def user_profile(request):
-    return None
+def user_profile(request, username):
+    user_profile = get_object_or_404(User, username=username)
+    if request.user == user_profile:
+        return redirect('profile', username=request.user.username)
+    params = {'user_profile': user_profile}
+    return render(request, 'z_awwards/user_profile.html', params)
 
 
-def search_profile(request):
-    return None
+@login_required(login_url='/login/')
+def user_projects(request, username):
+    """
+    User projects page
+    :param request:
+    :param username:
+    :return: user_projects page
+    """
+    user_profile = get_object_or_404(User, username=username)
+    if request.user == user_profile:
+        return redirect('profile', username=request.user.username)
+    params = {'user_profile': user_profile}
+    return render(request, 'z_awwards/user_projects.html', params)
+
+
+@login_required(login_url='/login/')
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        profile_form = UpdateUserProfileForm(request.POST, instance=request.user.userprofile)
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        if profile_form.is_valid() and user_form.is_valid():
+            profile_form.save()
+            user_form.save()
+            return redirect('profile', user.username)
+    else:
+        profile_form = UpdateUserProfileForm(request.POST, instance=request.user.userprofile)
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+    params = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+    }
+    return render(request, 'z_awwards/edit_profile.html', params)
+
