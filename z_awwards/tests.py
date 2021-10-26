adf63510f38c9ee7094ad776b68f7751a91e5c2e
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-26 07:09:47 +0300

    tests for Rating model

--- z_awwards/tests.py
+++ z_awwards/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 
 # Create your tests here.
-from z_awwards.models import UserProject
+from z_awwards.models import UserProject, ProjectRating
 
 
 class TestZooAwwards(TestCase):
@@ -101,3 +101,38 @@ class UserProjectTest(TestCase):
         self.user_project.delete_post()
         user_project = UserProject.search_project('test')
         self.assertTrue(len(user_project) < 1)
+
+
+class ProjectRatingTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='zken')
+        self.user_project = UserProject.objects.create(id=1, title='test user_project', photo="https://res.cloudinary.com/dd5ab8mp3/image/upload/v1634659738", description='desc',
+                                                       user=self.user, url='https://test.com')
+        self.rating = ProjectRating.objects.create(id=1, design=6, usability=7, content=9, user=self.user,
+                                                   project=self.user_project)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, ProjectRating))
+
+    def test_save_rating(self):
+        self.rating.save_rating()
+        rating = ProjectRating.objects.all()
+        self.assertTrue(len(rating) > 0)
+
+    def test_update_rating(self):
+        self.rating.design = 5
+        self.rating.save()
+        rating = ProjectRating.objects.get(id=1)
+        self.assertEquals(rating.design, 5)
+
+    def test_delete_rating(self):
+        self.rating.delete()
+        rating = ProjectRating.objects.all()
+        self.assertTrue(len(rating) < 1)
+
+
+    def test_get_user_project_rating(self):
+        id =1
+        self.rating.save()
+        rating = ProjectRating.get_ratings(project=id)
+        self.assertTrue(len(rating) == 1)
